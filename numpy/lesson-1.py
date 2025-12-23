5fdf21d81ef2173c2221e9995ec6f333b90ac03c
Ncy7 <chaudharynageshwor@gmail.com>
2025-12-23 21:38:29 +0545

    First lesson on numpy

---
+++ numpy/lesson-1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+my_list = [1, 2, 3, 4, 5]
+my_list = my_list * 2   # multiplies the list by 2 
+
+array = np.array([7,8,9])  # converts the list to a numpy array
+
+
+print(my_list)
+print(array)
+print(type(array))
